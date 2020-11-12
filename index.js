13cbd0dd746db14354b31efa9442b503e3e82625
Tim Holzer <timiholzer@gmail.com>
2020-11-11 18:13:31 -0700

    added comments to the js for what is required for the readme generator

--- index.js
+++ index.js
@@ -1,3 +1,19 @@
+// This readme generator requires a response about:
+
+//Title, usage, table of contents, description, installation, usage, license, rules for contributing, tests?, and questions?
+
+// maybe start with radio buttons to choose which of those types you want?
+
+//Title is required, license is from a list of options and added as a new file.
+
+//link is added for github username in the questions sections
+
+//email is added to the questions section
+
+//table of contents has links to different parts of the readme.
+
+
+
 // array of questions for user
 const questions = [
 
